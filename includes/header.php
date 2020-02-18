474b5b22dccb5afa9e188c58049d2947a1d8650e
DeniseLopes <denise@kosloski.com.br>
2020-02-18 11:41:46 -0300

    carousel

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,39 @@
  <!-- Header -->
- <div class="masthead-header">
-     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-         <div class="carousel-inner">
-             <div class="carousel-item active">
-                 <img class="d-block " src="img/header/img1.jpg" alt="First slide" style="height:850px; width:1600px">
+
+ <div id="carouselSite" class="carousel slide" data-ride="carousel">
+
+    <ol class="carousel-indicators">
+        <li data-target="carouselSite" data-slide-to="0" class="active"></li>
+        <li data-target="carouselSite" data-slide-to="1"></li>
+        <li data-target="carouselSite" data-slide-to="2"></li>
+    </ol>
+
+     <div class="carousel-inner">
+         <div class="carousel-item active">
+             <img class="d-block img-fluid " src="img/header/img1.jpg" alt="First slide" style="height:850px; width:1600px">
+             <div class="carousel-caption">
+                 <p>Romance</p>
              </div>
-             <div class="carousel-item">
-                 <img class="d-block " src="img/header/img2.jpg" alt="Second slide"style="height:850px; width:1600px">
+         </div>
+         <div class="carousel-item">
+             <img class="d-block img-fluid" src="img/header/img2.jpg" alt="Second slide" style="height:850px; width:1600px">
+             <div class="carousel-caption">
+                 <p>Romance</p>
              </div>
-             <div class="carousel-item">
-                 <img class="d-block " src="img/header/img3.jpg" alt="Third slide"style="height:850px; width:1600px">
+         </div>
+         <div class="carousel-item">
+             <img class="d-block img-fluid" src="img/header/img3.jpg" alt="Third slide" style="height:850px; width:1600px">
+             <div class="carousel-caption">
+                 <p>Romance</p>
              </div>
          </div>
      </div>
  </div>
+ <a class="carousel-control-prev" href="#carouselSite" role="button" data-slide="prev">
+     <span class="carousel-control-prev-icon"></span>
+     <span class="sr-only">Anterior</span>
+ </a>
+ <a class="carousel-control-next" href="#carouselSite" role="button" data-slide="next">
+     <span class="carousel-control-next-icon"></span>
+     <span class="sr-only">Avançar</span>
+ </a>
